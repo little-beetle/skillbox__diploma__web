e033563ae2663fc2a21cf7ccd68000727aa52912
Vladimir Kovalchuk <vvkovalchuk1409@gmail.com>
2022-09-22 23:21:27 +0300

    add properties css

--- js/index.js
+++ js/index.js
@@ -1,20 +1,45 @@
-let tabsBtn = document.querySelectorAll(".tabs-nav__btn");
-let tabsItem = document.querySelectorAll(".tabs-item");
+let search = document.querySelector(".header__search");
+let enter = document.querySelector(".enter-search");
 
-tabsBtn.forEach(function (element) {
-  element.addEventListener("click", function (e) {
-    const path = e.currentTarget.dataset.path;
+let exit__search = document.querySelectorAll(".exit");
+search.addEventListener("click", function () {
+  search.classList.toggle("header__search--active");
+  enter.classList.toggle("enter-search--active");
+  document.body.classList.toggle("stop-scroll");
+});
 
-    tabsBtn.forEach(function (btn) {
-      btn.classList.remove("tabs-nav__btn--active");
-    });
-    e.currentTarget.classList.add("tabs-nav__btn--active");
+exit__search.forEach(function (el) {
+  el.addEventListener("click", function () {
+    search.classList.remove("header__search--active");
+    enter.classList.remove("enter-search--active");
+    document.body.classList.remove("stop-scroll");
+  });
+});
 
-    tabsItem.forEach(function (element) {
-      element.classList.remove("tabs-item--active");
-    });
-    document
-      .querySelector(`[data-target="${path}"]`)
-      .classList.add("tabs-item--active");
+document.querySelector(".play__btn__1").addEventListener("click", function (e) {
+  e.target.closest(".play__btn__1").classList.toggle("play__btn__1--toggle");
+});
+document.querySelector(".play__btn__2").addEventListener("click", function (e) {
+  e.target.closest(".play__btn__2").classList.toggle("play__btn__2--toggle");
+});
+
+// let PlayBtn = document.querySelectorAll(".play__btn");
+// PlayBtn.forEach(function (element) {
+//   element.addEventListener("click", function (e) {
+//     e.target.closest(".play__btn").classList.toggle("play__btn--toggle");
+//   });
+// });
+let btnPlay = document.querySelectorAll(".btn__play");
+
+btnPlay.forEach(function (el) {
+  el.addEventListener("click", function (e) {
+    e.target.closest(".btn__play").classList.toggle("btn__play--toggle");
+  });
+});
+let btnLike = document.querySelectorAll(".like__btn");
+
+btnLike.forEach(function (el) {
+  el.addEventListener("click", function (e) {
+    e.target.closest(".like__btn").classList.toggle("like__btn--toggle");
   });
 });
