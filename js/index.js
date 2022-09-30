5fb14ffadc6f4dd5d61c5b3d219a6d6456075551
Vladimir Kovalchuk <vvkovalchuk1409@gmail.com>
2022-09-30 12:56:17 +0300

    fixing css and adding js properties

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+// пошук
+
 let search = document.querySelector(".header__search");
 let enter = document.querySelector(".enter-search");
 
@@ -16,6 +18,26 @@ exit__search.forEach(function (el) {
   });
 });
 
+let exitBtn = document.querySelector(".header__btn__enter");
+let enterBlock = document.querySelector(".enter");
+
+let outBtn = document.querySelectorAll(".close__btn");
+exitBtn.addEventListener("click", function () {
+  exitBtn.classList.toggle("header__enter--active");
+  enterBlock.classList.toggle("enter--active");
+  document.body.classList.toggle("stop-scroll");
+});
+
+outBtn.forEach(function (el) {
+  el.addEventListener("click", function () {
+    exitBtn.classList.remove("header__enter--active");
+    enterBlock.classList.remove("enter--active");
+    document.body.classList.remove("stop-scroll");
+  });
+});
+
+// зміна
+
 document.querySelector(".play__btn__1").addEventListener("click", function (e) {
   e.target.closest(".play__btn__1").classList.toggle("play__btn__1--toggle");
 });
@@ -23,12 +45,6 @@ document.querySelector(".play__btn__2").addEventListener("click", function (e) {
   e.target.closest(".play__btn__2").classList.toggle("play__btn__2--toggle");
 });
 
-// let PlayBtn = document.querySelectorAll(".play__btn");
-// PlayBtn.forEach(function (element) {
-//   element.addEventListener("click", function (e) {
-//     e.target.closest(".play__btn").classList.toggle("play__btn--toggle");
-//   });
-// });
 let btnPlay = document.querySelectorAll(".btn__play");
 
 btnPlay.forEach(function (el) {
@@ -42,4 +58,92 @@ btnLike.forEach(function (el) {
   el.addEventListener("click", function (e) {
     e.target.closest(".like__btn").classList.toggle("like__btn--toggle");
   });
+});
+
+//
+
+const btnMore = document.querySelector(".podcast__btn");
+const podcastItems = document.querySelectorAll(".podcast__item");
+
+btnMore.addEventListener("click", () => {
+  podcastItems.forEach((el) => {
+    el.classList.add("podcast__item--visible");
+  });
+  btnMore
+    .closest(".podcast__btn__block")
+    .classList.add("podcast__btn__block--hidden");
+});
+
+// select
+
+const element = document.querySelector("select");
+const choices = new Choices(element, {
+  searchEnabled: false,
+});
+
+new Accordion(".accordion-list", {
+  elementClass: "accordion",
+  triggerClass: "accordion__control",
+  panelClass: "accordion__content",
+  activeClass: "accordion--active",
+});
+
+const swiper = new Swiper(".swiper", {
+  slidesPerView: 4,
+  slidesPerGroup: 1,
+
+  spaceBetween: 30,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
+    clickable: true,
+  },
+});
+
+// form
+
+new JustValidate(".form", {
+  rules: {
+    name: {
+      required: true,
+      minLength: 2,
+      maxLength: 10,
+    },
+
+    mail: {
+      required: true,
+      email: true,
+    },
+  },
+  messages: {
+    name: "Ошибка",
+
+    mail: "Ошибка",
+  },
+});
+
+new JustValidate(".form__enter", {
+  rules: {
+    name: {
+      required: true,
+      minLength: 2,
+      maxLength: 10,
+    },
+
+    password: {
+      required: true,
+      minLength: 2,
+      maxLength: 10,
+    },
+  },
+  messages: {
+    name: "Ошибка",
+
+    password: "Ошибка",
+  },
 });
